a4af784e9e21850df7cc33c51118d5a075902574
Dovineowuor <owuordove@gmail.com>
2022-04-08 00:09:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - function begins
+ *
+ * Return: function ends
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
