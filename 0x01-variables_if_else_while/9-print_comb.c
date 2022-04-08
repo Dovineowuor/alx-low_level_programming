b8cffe45c3005bc0c9ad1937a174b583fe65fae7
Dovineowuor <owuordove@gmail.com>
2022-04-08 14:13:37 -0700

    9. Patience, persistence...

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-  int numb;
+int numb;
 
-  for (numb = '0' ; numb <= '9' ; numb++)
-    {
-      putchar(numb);
+for (numb = '0' ; numb <= '9' ; numb++)
+{
+putchar(numb);
 
-      if (numb != '9')
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+if (numb != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
