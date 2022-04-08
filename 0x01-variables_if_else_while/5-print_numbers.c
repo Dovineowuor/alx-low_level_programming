1d33d8ba970f671cc9f4d5a92295acce5565b1a3
Dovineowuor <owuordove@gmail.com>
2022-04-08 13:32:11 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-  int num;
+{int num;
 
-  for (num = 0 ; num <= 9 ; num++)
-    printf("%d", num);
-  puts("");
-  return (0);
+for (num = 0 ; num <= 9 ; num++)
+printf("%d", num);
+puts("");
+return (0);
 }
