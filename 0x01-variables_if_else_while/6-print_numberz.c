930544cfe42b6ef13ba733a2ff0408eb15a4d63d
Dovineowuor <owuordove@gmail.com>
2022-04-08 14:41:50 -0700

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,8 +8,8 @@ int main(void)
 {
 int num;
 
-  for (num = 48 ; num <= 57 ; num++)
-        putchar(num);
-         putchar('\n');
-  return (0);
+for (num = 48 ; num <= 57 ; num++)
+putchar(num);
+putchar('\n');
+return (0);
 }
