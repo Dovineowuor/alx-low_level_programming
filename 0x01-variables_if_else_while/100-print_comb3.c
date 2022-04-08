777a2ff40dc5d103e8bf605e64881ffea06c4016
Dovineowuor <owuordove@gmail.com>
2022-04-08 15:10:46 -0700

    10. Inventing is a combination of brains...

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - a simple program that outputs 0-9 separated by commas
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-  int n;
-  int m;
+int i;
+int j;
 
-  for (n = 48 ; n <= 57 ; n++)
-    {
-      for (m = 48 ; m <= 57 ; m++)
-	{
-	  putchar(n);
-	  putchar(m);
-
-	  if ((n != 57) || (m != 57))
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+for (i = 48; i < 57; i++)
+{
+for (j = i + 1; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
