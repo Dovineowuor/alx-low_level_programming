036c2a6a1ccb05c68841f9e7bf971847a91f8716
Dovineowuor <owuordove@gmail.com>
2022-04-08 12:14:00 -0700

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,8 +22,7 @@ n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n"
-n, n % 10);
+printf("Last digit of %d is %d and is less than 6 and not 0\n" n, n % 10);
 }
 else
 
