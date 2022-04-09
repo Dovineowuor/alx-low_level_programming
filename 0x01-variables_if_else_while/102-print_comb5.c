31e04db0d024541ca97433f7a4a1af41171b29c1
Dovineowuor <owuordove@gmail.com>
2022-04-08 22:01:14 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,37 +8,37 @@
  */
 int main(void)
 {
-  int x1;
-  int x2;
-  int y1;
-  int y2;
+int x1;
+int x2;
+int y1;
+int y2;
 
-  for (x1 = 0; x1 < 10; ++x1)
-    {
-      for (x2 = 0; x2 < 10; ++x2)
-	{
-	  for (y1 = x1; y1 < 10; ++y1)
-	    {
-	      for (y2 = ((x1 == y1) ? (x2 + 1) : 0); y2 < 10; ++y2)
-		{
-		  putchar('0' + x1);
-		  putchar('0' + x2);
-		  putchar(' ');
-		  putchar('0' + y1);
-		  putchar('0' + y2);
+for (x1 = 0; x1 < 10; ++x1)
+{
+for (x2 = 0; x2 < 10; ++x2)
+{
+for (y1 = x1; y1 < 10; ++y1)
+{
+for (y2 = ((x1 == y1) ? (x2 + 1) : 0); y2 < 10; ++y2)
+{
+putchar('0' + x1);
+putchar('0' + x2);
+putchar(' ');
+putchar('0' + y1);
+putchar('0' + y2);
 
-		  if (x1 != 9 || x2 != 8 ||
-		      y1 != 9 || y2 != 9)
-		    {
-		      putchar(',');
-		      putchar(' ');
-		    }
-		}
-	    }
-	}
-    }
+if (x1 != 9 || x2 != 8 ||
+y1 != 9 || y2 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
