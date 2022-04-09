7c23d9a01738f1219a1a800903f5fc58de3a0699
Dovineowuor <owuordove@gmail.com>
2022-04-08 21:52:11 -0700

    The success combination in business is: ...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,30 +8,30 @@
  */
 int main(void)
 {
-  int i;
-  int j;
-  int k;
+int i;
+int j;
+int k;
 
-  for (i = 0; i < 10; ++i)
-    {
-      for (j = i + 1; j < 10; ++j)
-	{
-	  for (k = j + 1; k < 10; ++k)
-	    {
-	      putchar('0' + i);
-	      putchar('0' + j);
-	      putchar('0' + k);
+for (i = 0; i < 10; ++i)
+{
+for (j = i + 1; j < 10; ++j)
+{
+for (k = j + 1; k < 10; ++k)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
 
-	      if (i != 7 || j != 8 || k != 9)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	    }
-	}
-    }
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
