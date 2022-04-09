ba246a14d5cfe41c0b4750a7449f37139a65ac76
Dovineowuor <owuordove@gmail.com>
2022-04-08 22:11:06 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - function begins
- *
- * Return: function ends
+ * main - entry point
+ * Description: Print a string using followed by a new line
+ * Return: 0
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
