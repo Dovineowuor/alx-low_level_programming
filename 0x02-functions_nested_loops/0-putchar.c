23366271512085c47fb48b0f296bdb968f4b554f
Dovineowuor <owuordove@gmail.com>
2022-04-12 03:45:21 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,20 @@
 #include "main.h"
 
 /**
- * main - Print the word "Holberton"
- *
- * Return: Always 0 (Success)
- */
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-  char h[] = "ALx Software engineering";
-  int i;
+char h[] = "ALx Software engineering";
+int i;
 
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(h[i]);
-    }
-  _putchar('\n');
-  return (0);
+for (i = 0; i < 9; i++)
+{
+_putchar(h[i]);
+}
+_putchar('\n');
+return (0);
 }
