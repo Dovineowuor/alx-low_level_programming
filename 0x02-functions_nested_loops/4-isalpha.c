b40b177358b57f0391058075ff4ae47c891c85ce
Dovineowuor <owuordove@gmail.com>
2022-04-12 08:34:59 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-* _isalpha - alphabet case checker. Return  if true and 0 if false
-* @c: integer parameter
-* Return: Returns value
-*/
+ *  * _isalpha - alphabet case checker. Return  if true and 0 if false
+ *   * @c: integer parameter
+ *    * Return: Returns value
+ *     */
 
 int _isalpha(int c)
 {
-int value;
+	int value;
 
-if ((c >= 'A' || c >= 'a') && (c <= 'Z' || c <= 'z'))
-{
-value = 1;
-}
-else
-
-value = 0;
-}
-return (value);
+	if ((c >= 'A' || c >= 'a') && (c <= 'Z' || c <= 'z'))
+	{
+			value = 1;
+		}
+	else
+	{
+			value = 0;
+		}
+	return (value);
 }
