bec9fc24f45ccd1e8e67fea32864b1e33ee85222
Dovineowuor <owuordove@gmail.com>
2022-04-12 08:09:46 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,16 +5,37 @@
 *
 * Return: Always 0.
 */
-int main(void)
-{
-int c;
+/**
+*int main(void)
+*{
+*int c;
 
-c = _islower('p');
-_putchar(c + 'u');
-c = _islower('t');
-_putchar(c + 'c');
-c = _islower('h');
-_putchar(c + 'a');
-_putchar(c + 'r');                                                                                                      _putchar('\n');
+*c = _islower('p');
+*_putchar(c + 'u');
+*c = _islower('t');
+*_putchar(c + 'c');
+*c = _islower('h');
+*_putchar(c + 'a');
+*_putchar(c + 'r');                                                                                                      *_putchar('\n');
+*return (0);
+*}
+*/
+#include "main.h"
+
+/**
+ * _islower - Short descritpion, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
 return (0);
+}
 }
