e636e406e7f8340d31963819a04e9fc3af9d63e4
Dovineowuor <owuordove@gmail.com>
2022-04-19 18:37:40 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,40 @@
+Skip to content
+josephtam-github
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+More
+alx-low_level_programming/0x04-more_functions_nested_loops/3-main.c
+
+Joseph Tam 3-print_numbers file
+ History
+ 0 contributors
+23 lines (15 sloc)  175 Bytes
+#include "main.h"
+
+
+
+/**
+*
+*  * main - check the code
+*
+*   *
+*
+*    * Return: Always 0.
+*
+*     
+*/
+
+int main(void)
+{
+
+print_numbers();
+
+return (0);
+
+}
