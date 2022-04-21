6eb743d46499e2c5047d09416336f6bb5e271598
Dovineowuor <owuordove@gmail.com>
2022-04-21 03:52:09 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy strg to another string
+ * @dest: pointer
+ * @src: pointer
+ *
+ * Return: numbers dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
