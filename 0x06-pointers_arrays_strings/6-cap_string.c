738883d3179e8221018bcd069fb9ad2124df99cf
Dovineowuor <owuordove@gmail.com>
2022-04-23 23:53:47 -0700

    6. all the strings in capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,36 @@
 #include "main.h"
 /**
-*cap_string - This is the main function
-*@s: This is the string to be entered
-*Return: void
-*/
+ *cap_string - This is the main function
+ *@s: This is the string to be entered
+ *Return: void
+ */
 char *cap_string(char *s)
 {
-int index = 0;
+  int index = 0;
 
-while (s[index])
-{
-while (!(s[index] >= 'a' && s[index] <= 'z'))
-index++;
+  while (s[index])
+    {
+      while (!(s[index] >= 'a' && s[index] <= 'z'))
+	index++;
 
-if (s[index - 1] == ' ' ||
-s[index - 1] == '\t' |
-s[index - 1] == '\n' ||
-s[index - 1] == ',' ||
-s[index - 1] == ';' ||
-s[index - 1] == '.' ||
-s[index - 1] == '!' ||
-s[index - 1] == '?' ||
-s[index - 1] == '"' ||
-s[index - 1] == '(' ||
-s[index - 1] == ')' ||
-s[index - 1] == '{' ||
-s[index - 1] == '}' ||
-index == 0)
-s[index] -= 32;
+      if (s[index - 1] == ' ' ||
+	      s[index - 1] == '\t' ||
+	      s[index - 1] == '\n' ||
+	  s[index - 1] == ',' ||
+	  s[index - 1] == ';' ||
+	      s[index - 1] == '.' ||
+	      s[index - 1] == '!' ||
+	      s[index - 1] == '?' ||
+	      s[index - 1] == '"' ||
+	  s[index - 1] == '(' ||
+	  s[index - 1] == ')' ||
+	  s[index - 1] == '{' ||
+	  s[index - 1] == '}' ||
+	  index == 0)
+	s[index] -= 32;
 
-index++;
-}
+      index++;
+    }
 
-return (s);
+  return (s);
 }
