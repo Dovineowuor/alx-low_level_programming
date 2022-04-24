70a1fbb87d769682c0a31cdc209bb1409accd5f8
Dovineowuor <owuordove@gmail.com>
2022-04-24 01:24:05 -0700

    3. strcmp; This program compares two or more strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-*_strcmp - This is the fucnction for performing the comparison
-*@s1: This the first string
-*@s2: This is the second comparison
-*Return: void
-*/
+ *_strcmp - This is the fucnction for performing the comparison
+ *@s1: This the first string
+ *@s2: This is the second comparison
+ *Return: void
+ */
 int _strcmp(char *s1, char *s2)
 {
-int length;
+  int length;
 
-length = strcmp(s1, s2);
-_putchar(length);
+  length = strcmp(s1, s2);
+  _putchar(length);
 
-return (0);
+  return (0);
 }
