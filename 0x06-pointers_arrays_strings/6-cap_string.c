e5977199227f421820b41003be68ddccb5d29746
Dovineowuor <owuordove@gmail.com>
2022-04-23 23:51:05 -0700

    6. all the strings in capital

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+include "main.h"
+/**
+*cap_string - This is the main function
+*@s: This is the string to be entered
+*Return: void
+*/
+char *cap_string(char *s)
+{
+int index = 0;
+
+while (s[index])
+{
+while (!(s[index] >= 'a' && s[index] <= 'z'))
+index++;
+
+if (s[index - 1] == ' ' ||
+s[index - 1] == '\t' |
+s[index - 1] == '\n' ||
+s[index - 1] == ',' ||
+s[index - 1] == ';' ||
+s[index - 1] == '.' ||
+s[index - 1] == '!' ||
+s[index - 1] == '?' ||
+s[index - 1] == '"' ||
+s[index - 1] == '(' ||
+s[index - 1] == ')' ||
+s[index - 1] == '{' ||
+s[index - 1] == '}' ||
+index == 0)
+s[index] -= 32;
+
+index++;
+}
+
+return (s);
+}
