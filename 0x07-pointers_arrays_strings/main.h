50cc099e1034c8ed3bfdc78cac673dc12c697c44
Dovineowuor <owuordove@gmail.com>
2022-04-26 14:31:54 -0700

    0. memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,17 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef main
+#define main
+
+/**
+ *File:main
+ *
+ *Description: This file is the main header file.\n all the declarations of all the functions appliedd in the program
+ */
+
+#include <string.h>
+#include <stdio.h>
+
 int _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,4 +21,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+
 #endif
