027c4ae23b5c86e58dad790c1db7b02393ca86c0
Dovineowuor <owuordove@gmail.com>
2022-04-26 14:53:42 -0700

    2. strchr:This function locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_strchr - The main function locating a character in a string
+*@s: Thee destination pointer
+*@c: This the source character
+*Return:  resturns to the destination pointer
+*/
+char *_strchr(char *s, char c)
+{
+char *ret = strchr(s, c);
+
+return (ret);
+}
