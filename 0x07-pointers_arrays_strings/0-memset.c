6ae0d8b0233ce248e336a4a25cd1b54df7a7672e
Dovineowuor <owuordove@gmail.com>
2022-04-26 14:25:29 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,13 @@
 #include "main.h"
-
 /**
-* _memset - fills string with constant char b upto n bytes
-* @s: input pointer to string
-* @b: constant char
-* @n: number of bytes
-* Return: pointer to s string
+*_memset - This is the main function for setting the memory
+*@s: this is pointer
+*@b: the is the character
+*@n: The is the unassigned character
+*Return: Returns the memeory of s
 */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
-
-while (i < n)
-{
-s[i] = b;
-i++;
+memset(s, b, n);
 }
-return (s);
-}
+return(s)
