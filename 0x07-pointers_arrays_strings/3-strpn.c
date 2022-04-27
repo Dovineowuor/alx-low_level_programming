0eba80c8b71d09fd7318dde7122d7de898808d6a
Dovineowuor <owuordove@gmail.com>
2022-04-27 03:27:46 -0700

    3. strpn;This file has  function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strpn.c
+++ 0x07-pointers_arrays_strings/3-strpn.c
@@ -1,24 +1,26 @@
 #include "main.h"
 /**
-* _strspn - prints the consecutive caracters of s1 that are in s2.
-* @s: source string
-* @accept: searching string
-*
-* Return: new string.
+ * _strspn - returns number of bytes that much specified criteria
+* @s: string to search
+* @accept:Accept search string
+* Return: number of bytes matching search criteria
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
+int i, j, k = 0;
 
-for (j = 0; *(s + j); j++)
+for (i = 0; *(s + i); i++)
 {
-for (i = 0; *(accept + i); i++)
+for (j = 0; *(accept + j); j++)
 {
-if (*(s + j) == *(accept + i))
+if (*(s + i) == *(accept + j))
+{
+k += 1;
 break;
 }
-if (*(accept + i) == '\0')
+}
+if (*(s + i) != *(accept + j))
 break;
 }
-return (j);
+return ((unsigned int) k);
 }
