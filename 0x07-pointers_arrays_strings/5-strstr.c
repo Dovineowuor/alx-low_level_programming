975ab9f10b464d64774584639c0e8c1b314d9746
Dovineowuor <owuordove@gmail.com>
2022-04-27 04:21:59 -0700

    5. strstr;  a function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,31 @@
 #include "main.h"
+
 /**
-* _strstr - prints the consecutive caracters of s1 that are in s2.
-* @haystack: source string
-* @needle: searching string
-*
-* Return: new string.
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
 */
 char *_strstr(char *haystack, char *needle)
 {
-while (*haystack)
-{
-if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-{
-return (haystack);
-}
-else
-{
-haystack++;
-}
-}
-return (0);
-}
-/**
-* coincidence - define if the string b is inside a.
-* @a: source string
-* @b: string to be searched
-*
-* Return: 1 if there is coincidence, otherwise 0.
-*/
-int coincidence(char *a, char *b)
-{
-while (*b && *b == *a)
-{
-b++;
-a++;
-}
+	char *bhaystack;
+	char *pneedle;
 
-if (*b == '\0')
-return (1);
-else
-return (0);
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
 }
