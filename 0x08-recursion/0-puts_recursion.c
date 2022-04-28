d8eb8b438df3cc9e854878f4e114e617e5ef9515
Dovineowuor <owuordove@gmail.com>
2022-04-28 10:18:14 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget; Prints a string and a new empty line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
- * _puts_recursion - Prints a string, followed by a new line
- * @s: the string to be printed
+ * _puts_recursion - prints a string and new line to stdout
+ * @s: string to print
+ * Return: Nothing
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\n');
-		_puts_recursion(++s)
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(++s);
 }
