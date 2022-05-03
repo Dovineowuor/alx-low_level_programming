d5ff3ce06f0bd72ed107cdcb2de235afb71fdfe3
Dovineowuor <owuordove@gmail.com>
2022-05-03 15:31:36 -0700

    0x09 Nine new Copy Files

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute balue of an integer
+* @n: integer type parameter
+* Return: return 0
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = (-1) * n;
+}
+return (n);
+}
