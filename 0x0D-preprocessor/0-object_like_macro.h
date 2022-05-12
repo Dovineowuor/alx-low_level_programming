07650ceb140e42e9bd9edd9dbaab5fce9babea75
Dovineowuor <owuordove@gmail.com>
2022-05-12 03:12:57 -0700

    0. Object-like macro: Create a header file that defines a macro named SIZE as an abbreviation for the token 1024.

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -5,7 +5,7 @@
 * #ifndef _OBJECT_LIKE_MACRO_: Checks if -OBJECT_LIKE_MACRO*_ is defined
 *#define _OBJECT_LIKE_MACRO_ : Defines -OBJECT_LIKE_MACRO_
 */
-#define BUFFER_SIZE 1024
+#define SIZE 1024
 
 #endif
 
