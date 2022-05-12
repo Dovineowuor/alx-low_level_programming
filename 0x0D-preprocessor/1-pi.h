fc15ea4c54ee768b43c3e188b69137d3a6eb30eb
Dovineowuor <owuordove@gmail.com>
2022-05-12 05:00:43 -0700

    1. PiCreate a header file that defines a macro named PI as an abbreviation for the token 3.14159265359

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,10 @@
-#ifndef P_
+#ifndef PI
 #define PI
 
 /*
- * #ifndef PI : Checks if _PI_ IS DEFINED
- * #define PI : Defines  _PI_  
+* #ifndef PI : Checks if PI IS DEFINED
+* #define PI : Defines  PI  
+*/
 #define PI 3.14159265359
 
 #endif
