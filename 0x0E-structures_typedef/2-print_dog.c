3bc05574c4ebb0c5a343c6bd0c87c862e36739a8
Dovineowuor <owuordove@gmail.com>
2022-05-12 04:41:47 -0700

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad: Write a function that prints a struct dog
    Prototype: void print_dog(struct dog *d);
    Format: see example bellow
    You are allowed to use the standard library
    If an element of d is NULL, print (nil) instead of this element. (if name is NULL, print Name: (nil))
    If d is NULL print nothing.

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,29 +1,22 @@
-2-print_dog.c:
-
 #include "dog.h"
 #include <stdio.h>
 
 /**
- * print_dog - prints a struct dog.
- * @d: struct dog.
- *
- * Return: no return.
+ * print_dog -entry point
+ * @d: Entry of struct
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		if (!(d->name))
-			printf("Name: (nil)\n");
+		if (d->name == NULL)
+			printf("Name: %p\n", (void *)d->name);
 		else
-			printf("Name: %s\n", d->name);
-
+			printf("Name: %s\n",  d->name);
 		printf("Age: %f\n", d->age);
-
-		if (!(d->owner))
-			printf("Owner: (nil)\n");
+		if (d->owner == NULL)
+			printf("Owner: %p\n", (void *)d->owner);
 		else
 			printf("Owner: %s\n", d->owner);
 	}
 }
-
