93084cca2b449416619730348304a7eb46634319
Dovineowuor <owuordove@gmail.com>
2022-05-16 15:20:27 -0700

    Readme file

---
+++ .git<0-object_like_macro.h
@@ -0,0 +1,19 @@
+Object like macro
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+# Your branch is ahead of 'origin/master' by 2 commits.
+#   (use "git push" to publish your local commits)
+#
+# Changes to be committed:
+#	modified:   0-object_like_macro.h
+#
+# Changes not staged for commit:
+#	deleted:    ../1-string_nconcat.c
+#	modified:   ../alx-low_level_programming (modified content)
+#	deleted:    ../main.h
+#
+# Untracked files:
+#	../.git<main.h
+#
