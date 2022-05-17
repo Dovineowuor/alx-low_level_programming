5cbc74296d7089d1bffcb55ae2c43fc6cae0d7a5
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-05-17 18:33:24 +0300

    Correction on function_poinyers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _FUNCTION_FILE_H_
 /*Function_pointers.h is the header file gor this repo
 *It defines the basic functions and their pointers.
+*/
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
