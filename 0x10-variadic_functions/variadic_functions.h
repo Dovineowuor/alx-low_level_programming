33488b745480ce95df5ad6fa2becbaeed1bcc787
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-05-17 23:37:48 +0300

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef VAR_FUNCS_H
+#define VAR_FUNCS_H
+/*
+* This is the main variadic header file
+*@ifndef VAR_FUNCS_H: defines the variadic functions header
+*/
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VAR_FUNCS_H */
