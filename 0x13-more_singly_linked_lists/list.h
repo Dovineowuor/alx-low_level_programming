073c8566fd83118b5ca229b8c36f71478216a28f
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-05-29 16:41:01 +0300

    update main list header file

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,68 +1,28 @@
-#ifndef LIST_H
-#define  LIST_H
-#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Auth: Dovine OWuor
+ * Desc: Header file containing prototypes
+ */
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next node
+ * @next: It points to the next node
  *
  * Description: singly linked list node structure
- *
-<<<<<<< HEAD
- *      * Description: singly linked list node structure
- *
- *       * for ALX project
- *
- *        */
-/**
- *
- *  * struct listp_s - singly linked list
- *
- *   * @p: pointers of nodes
- *
- *    * @next: points to the next node
- *
- *     *
- *
- *      * Description: singly linked list of pointers
- *	
- *
- *
- *  * struct listint_s - singly linked list
- *
- *   * @n: integer
- *
- *    * @next: points to the next node
- *
- *     *
- *
- *      * Description: singly linked list node structure
- *
- *       *
- *
- *        */
-
-typedef struct listint_s
-
-{
-
-	int n;
-
-	struct listint_s *next;
-
-} listint_t;
-
-
-
-=======
+ * for ALX SE project
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
->>>>>>> 16814b194e5bf23b5ae5b42edab1a2c107434dde
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -74,8 +34,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-<<<<<<< HEAD
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-=======
->>>>>>> 16814b194e5bf23b5ae5b42edab1a2c107434dde
 #endif
