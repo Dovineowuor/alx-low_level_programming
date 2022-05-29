20996a32e0827b21238db79e0b30e3c0b9c09cbc
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-05-29 16:34:41 +0300

    task5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  *free_listint - frees listint_t list.
+  *@head: pointer to listint_t list.
+  */
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+			free_listint(head->next);
+
+		free(head);
+	}
+}
