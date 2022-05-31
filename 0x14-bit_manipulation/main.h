4817dbcc9f1d1fe99d8e3680015eb66284b83c9b
owuordove@gmail.com <owuordove@gmail.com>
2022-05-31 15:49:51 +0300

    MAIN HEADER FILE

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,4 +10,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* _N_H_ */
+#endif /* _MAIN_H_ */
