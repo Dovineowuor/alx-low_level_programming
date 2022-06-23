f2b3df3fd7bf20dd86692816625ae908d31c8a28
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2022-06-23 11:33:05 +0300

    Update 2-append_text_to_file.c

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -13,27 +13,33 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int ID = 0, wr = 0, count = 0;
+	int file, wr, i = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	if (text_content != NULL)
-	{
-		for (count = 0; text_content[count] != '\0'; count++)
-		{}
-	}
-	ID = open(filename, O_APPEND | O_WRONLY, 0600);
-	if (ID == -1)
-	{
+
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
 		return (-1);
+
+
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
 	}
-	wr = write(ID, text_content, count);
+	else
+	{
+		wr = write(file, text_content, i);
+	}
+
 	if (wr == -1)
-	{
 		return (-1);
-	}
-	close(ID);
+
+	close(file);
 	return (1);
 }
