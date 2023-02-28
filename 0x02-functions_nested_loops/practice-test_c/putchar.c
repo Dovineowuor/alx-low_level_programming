e963d411f8276c7844b723ad427d1b1ba4fd9a30
Dovineowuor <owuordove@gmail.com>
2023-02-27 22:33:41 -0800

    putchar

---
+++ 0x02-functions_nested_loops/practice-test_c/putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include"_putchar.c"
+#include "putchar.h"
+/*
+*putchar: A function that prints putchar
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
