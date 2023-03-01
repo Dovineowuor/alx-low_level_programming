d79468aa4292a809965fcb8ef8cd67e33cf56715
Dovineowuor <owuordove@gmail.com>
2023-03-01 07:33:18 -0800

    1 main c

---
+++ 0x05-pointers_arrays_strings/tutorials-test_c/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - addresses of the variables
+ *
+ * Return: Always Zero, 0.
+ */
+
+int main()
+{
+char c;
+int n;
+
+
+printf("Address of var 'c': %p\n", &c);
+printf("The address of 'n': %p\n", &n);
+return (0);
+}
